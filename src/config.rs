c1e5f8f1d0f762f96f77799ab56eab4ff2b7248d
Nikaidou Haruki <nharuki@pm.me>
2026-02-09 00:16:24 +0900

    define dynamic speedlimit

--- src/config.rs
+++ src/config.rs
@@ -4,8 +4,11 @@ use reqwest::Url;
 pub struct ServiceConfig {
     /// The lambda of the exponential distribution.
     pub lambda: f64,
+
+    #[serde(default)]
     /// The speed limit in bytes per second.
-    pub speed_limit: u64,
+    pub speed_limit: Option<SpeedLimit>,
+    
     /// The sources to download from.
     pub source: Vec<Source>,
 }
@@ -19,4 +22,17 @@ pub struct Source {
 
 fn default_weight() -> f64 {
     1.0
+}
+
+#[derive(Debug, Clone, serde::Deserialize)]
+#[serde(tag = "type", content = "value")]
+pub enum SpeedLimit {
+    Static(u64),
+    Dynamic(Vec<DynamicSpeedLimitPoint>),
+}
+
+#[derive(Debug, Clone, serde::Deserialize)]
+pub struct DynamicSpeedLimitPoint {
+    pub time: time::Time,
+    pub speed_limit: u64,
 }
